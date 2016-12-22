d5b77b438d915317064ce532d5798fd6c20cafbe
Vihan B <contact@vihan.org>
2016-12-21 16:24:10 -0800

    Fixed bug where duplicate import targets would be overriden
    Signed-off-by: Vihan B <contact@vihan.org>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export default function (babel) {
     const { types: t } = babel;
 
     return {
-        name: "ast-transform", // not required
         visitor: {
             FunctionDeclaration(path) {
                 try {
@@ -17,18 +16,20 @@ export default function (babel) {
                     if (path.node.declarations[0].init.callee.name == "_interopRequireDefault") {
                         var dec = path.node.declarations[0];
                         var name = dec.id.name;
-                        var id = name.slice(0, -1);
-                        if (/2$/.test(dec.id.name)) {
-                            path.scope.path.traverse({
-                                MemberExpression(path2) {
-                                    if (path2.node.object.name === name && path2.node.property.name === "default") {
-                                        path2.replaceWith(
-                                                t.Identifier(id)
-                                                );
-                                    }
+                        var nameParts = /(.+)(\d+)$/.exec(name);
+                        var num = nameParts[2] - 1 < 2 ? "" : nameParts[2] - 1;
+                        var id = nameParts[1] + num;
+
+                        path.scope.path.traverse({
+                            MemberExpression(path2) {
+                                if (path2.node.object.name === name && path2.node.property.name === "default") {
+                                    path2.replaceWith(
+                                        t.Identifier(id)
+                                    );
                                 }
-                            });
-                        }
+                            }
+                        });
+
                         path.remove();
                     }
                 } catch(e) {}
@@ -36,3 +37,6 @@ export default function (babel) {
         }
     };
 }
+
+
+
